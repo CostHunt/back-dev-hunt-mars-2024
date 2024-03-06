50821e2ae3dfab7259d737f386b5ce00ec52807f
Nan6n <ralaivaonancy52@gmail.com>
2024-03-06 13:42:20 +0200

    Post CRUD

--- routes/main.route.js
+++ routes/main.route.js
@@ -2,11 +2,13 @@ const express = require('express');
 const fileRoute = require('./file.route');
 const quartierRoute = require('../routes/quartier.rourte');
 const projectRoute = require('../routes/project.route');
+const postRoute = require('../routes/post.route');
 const router = express.Router();
 const authRoute = require('./auth.route');
 router.use('/auth', authRoute);
 router.use('/file',fileRoute);
 router.use('/quartier',quartierRoute);
 router.use('/project',projectRoute);
+router.use('/post',postRoute);
 
 module.exports = router;
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const postController = require('../controllers/post.controller');
+const authController = require('../controllers/auth.controller');
 
 const router = express.Router();
 
 // Define routes for CRUD operations on posts
+router.use(authController.verifyToken);
 router.post('/', postController.createPost);
 router.get('/', postController.getPosts);
 router.get('/:id', postController.getPostById);
